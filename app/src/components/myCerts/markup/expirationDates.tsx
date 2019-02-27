d77b7bdfe68603364908651abaeaf695305a266a
ssskram <paul.marks@pittsburghpa.gov>
2019-02-27 12:26:06 -0500

    icc, ucc functions

--- app/src/components/myCerts/markup/expirationDates.tsx
+++ app/src/components/myCerts/markup/expirationDates.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import * as types from '../../../store/types'
+import iccExp from '../functions/ICCexpiration'
+import uccExp from '../functions/UCCexpiration'
 
 type props = {
     certifications: types.certification[]
@@ -16,9 +18,11 @@ export default class ExpirationDates extends React.Component<props, {}> {
             <div style={{ margin: '25px 0px' }} className='row text-center'>
                 <div className='col-md-6'>
                     <h4><u>ICC Expiration</u></h4>
+                    <h4><b>{iccExp(this.props.certHistory, this.props.certifications)}</b></h4>
                 </div>
                 <div className='col-md-6'>
                     <h4><u>UCC Expiration</u></h4>
+                    <h4><b>{uccExp(this.props.certHistory, this.props.certifications)}</b></h4>
                 </div>
             </div>
         )
