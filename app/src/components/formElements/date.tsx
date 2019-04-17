35fb67187925f5dca9b68efc4390907c5e3bbea7
ssskram <paul.marks@pittsburghpa.gov>
2019-04-17 12:55:43 -0400

    fixerd date input formatting, cleave

--- app/src/components/formElements/date.tsx
+++ app/src/components/formElements/date.tsx
@@ -1,22 +1,30 @@
-import * as React from 'react'
-import Cleave from 'cleave.js/react'
+import * as React from "react";
+import Cleave from "cleave.js/react";
 
 export default class Date extends React.Component<any, any> {
-
-    public render() {
-        return (
-            <div className="form-group">
-                <div className="col-md-12 form-element">
-                    <h5 className="form-h4">{this.props.header}{this.props.required == true && <span style={{ color: 'red', fontSize: '20' }}>*</span>}</h5>
-                    <Cleave type='search'
-                        className='form-control'
-                        options={{ date: true }}
-                        value={this.props.value}
-                        placeholder={this.props.placeholder}
-                        onChange={this.props.callback.bind(this)}>
-                    </Cleave>
-                </div>
-            </div >
-        )
-    }
+  public render() {
+    return (
+      <div className="form-group">
+        <div className="col-md-12 form-element">
+          <h5 className="form-h4">
+            {this.props.header}
+            {this.props.required == true && (
+              <span style={{ color: "red", fontSize: "20" }}>*</span>
+            )}
+          </h5>
+          <Cleave
+            type="search"
+            className="form-control"
+            options={{
+              date: true,
+              datePattern: ["m", "d", "Y"]
+            }}
+            value={this.props.value}
+            placeholder={this.props.placeholder}
+            onChange={this.props.callback.bind(this)}
+          />
+        </div>
+      </div>
+    );
+  }
 }
