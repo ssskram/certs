3b4878bc11149ee43c093ee12f943c07ac04c38b
ssskram <paul.marks@pittsburghpa.gov>
2019-05-13 21:33:22 -0400

    prettify

--- app/src/components/nav/index.tsx
+++ app/src/components/nav/index.tsx
@@ -1,24 +1,23 @@
-import * as React from 'react'
-import { connect } from 'react-redux'
-import { ApplicationState } from '../../store'
-import * as userProfile from '../../store/userProfile'
-import * as types from '../../store/types'
-import { Link } from 'react-router-dom'
-import { Nav, NavItem, Navbar } from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap'
-import Account from './userProfile'
+import * as React from "react";
+import { connect } from "react-redux";
+import { ApplicationState } from "../../store";
+import * as userProfile from "../../store/userProfile";
+import * as types from "../../store/types";
+import { Link } from "react-router-dom";
+import { Nav, NavItem, Navbar } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import Account from "./userProfile";
 
 const btnStyle = {
-  fontSize: '16px',
-  margin: '6px 10px'
-} 
+  fontSize: "16px",
+  margin: "6px 10px"
+};
 
 type props = {
-  userProfile: types.userProfile
-}
+  userProfile: types.userProfile;
+};
 
 export class NavMenu extends React.Component<props, {}> {
-
   public render() {
     return (
       <Navbar
@@ -30,22 +29,29 @@ export class NavMenu extends React.Component<props, {}> {
       >
         <Navbar.Header>
           <Navbar.Brand>
-            <Link to={'/'}>PGH Certs</Link>
+            <Link to={"/"}>PGH Certs</Link>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
-        <Navbar.Collapse className='text-xs-center'>
-          {this.props.userProfile.isAdmin &&
+        <Navbar.Collapse className="text-xs-center">
+          {this.props.userProfile.isAdmin && (
             <Nav>
-              <LinkContainer to={'/Admin'}>
-                <NavItem><button className='btn btn-danger nav-button' style={btnStyle}>Admin</button></NavItem>
+              <LinkContainer to={"/Admin"}>
+                <NavItem>
+                  <button
+                    className="btn btn-danger nav-button"
+                    style={btnStyle}
+                  >
+                    Admin
+                  </button>
+                </NavItem>
               </LinkContainer>
             </Nav>
-          }
+          )}
           <Account />
         </Navbar.Collapse>
       </Navbar>
-    )
+    );
   }
 }
 
@@ -53,7 +59,7 @@ export default connect(
   (state: ApplicationState) => ({
     ...state.userProfile
   }),
-  ({
+  {
     ...userProfile.actionCreators
-  })
-)(NavMenu as any)
+  }
+)(NavMenu as any);
